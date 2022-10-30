ed76c59f01d0d7cb2bc2bb4488cde95e3becee51
rockstararyaman <93260229+rockstararyaman@users.noreply.github.com>
2022-10-30 09:49:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(500,440);
+    canvas.center();
+}
+
+function draw () {
+
+}
+
+function take_snapshot() {
+    save('filter_mustache_image');
+}
